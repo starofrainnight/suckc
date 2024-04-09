c8a16b0216b81111a77f654484fef501156cbcdd
Hong-She Liang <sorn@taorules.com>
2024-04-09 22:02:10 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SuckC compiler
+
+SuckC is a simple compiler of SuckC language which base of C&C++ language
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/**
+ * Main entry point of the program.
+ *
+ * @param argc Number of arguments, representing the count of command line
+ * arguments passed in.
+ * @param argv Array of pointers to character arrays, each character array
+ * representing a single command line argument.
+ * @return The function always returns 0, indicating normal program termination.
+ */
+int main(int argc, char *argv[]) {
+  // Output greeting message
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
