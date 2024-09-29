c3e1754a711948c09fc9f40193eb0d21192621c1
Hong-She Liang <sorn@taorules.com>
2024-09-29 23:17:04 +0800

    Introduce wxWidgets as base utility

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,33 @@
+
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+// for all others, include the necessary headers (this file is usually all you
+// need because it includes almost all "standard" wxWidgets headers)
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
 #include "SourceGenerator.h"
 #include "SuckCLexer.h"
 #include "SuckCParser.h"
 #include <antlr4-runtime.h>
 #include <fstream>
 #include <iostream>
+#include <wx/app.h>
+#include <wx/cmdline.h>
 
-/**
- * Print usage information
- */
-void printUsage() { std::cout << "Usage: suckc <input file>" << std::endl; }
+// struct test demo[] = {{56, 23, "hello"}, {-1, 93463, "world"}, {7, 5, "!!"}};
+
+static const wxCmdLineEntryDesc cmdLineDesc[] = {
+    {wxCMD_LINE_SWITCH, "h", "help", "show this help message",
+     wxCMD_LINE_VAL_NONE, wxCMD_LINE_OPTION_HELP},
+    {wxCMD_LINE_SWITCH, "d", "debug", "debug the ast tree", wxCMD_LINE_VAL_NONE,
+     0},
+    {wxCMD_LINE_PARAM, nullptr, nullptr, "SRC_FILE", wxCMD_LINE_VAL_STRING, 0},
+    wxCMD_LINE_DESC_END,
+};
 
 /**
  * Main entry point of the program.
@@ -20,18 +39,36 @@ void printUsage() { std::cout << "Usage: suckc <input file>" << std::endl; }
  * @return The function always returns 0, indicating normal program termination.
  */
 int main(int argc, char *argv[]) {
-  // Read the file specific by first argument
-  if (argc < 2) {
-    printUsage();
-    return 1;
+  wxApp::CheckBuildOptions(WX_BUILD_OPTIONS_SIGNATURE, "program");
+
+  wxInitializer initializer;
+  if (!initializer) {
+    fprintf(stderr, "Failed to initialize the wxWidgets library, aborting.");
+    return -1;
   }
 
-  std::cout << "Parsing file: " << argv[1] << std::endl;
+  bool isDebug = false;
 
-  std::ifstream stream(argv[1]);
+  wxCmdLineParser cmdLineParser(cmdLineDesc, argc, argv);
+  auto ret = cmdLineParser.Parse();
+  if (-1 == ret) {
+    return -2;
+  }
+
+  if (argc == 1) {
+    // If there were no command-line options supplied, emit a message
+    // otherwise it's not obvious that the sample ran successfully
+    return -3;
+  }
+
+  auto fpath = cmdLineParser.GetParam(0);
+
+  std::cout << "Parsing file: " << fpath.c_str().AsChar() << std::endl;
+
+  std::ifstream stream(fpath.c_str().AsChar());
   if (!stream) {
     std::cerr << "Failed to open input file." << std::endl;
-    return 1;
+    return -4;
   }
 
   antlr4::ANTLRInputStream input(stream);
