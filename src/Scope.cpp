8b0f15346853dc2397012f424eb0ccf2a380223f
Hong-She Liang <sorn@taorules.com>
2025-02-10 22:44:18 +0800

    Fixed print nodes elements without correct orders

--- src/Scope.cpp
+++ src/Scope.cpp
@@ -1,5 +1,6 @@
 #include "Scope.h"
 #include "ast/Function.h"
+#include <list>
 #include <unordered_map>
 
 namespace suckc {
@@ -9,9 +10,11 @@ class ScopePrivate {
 
 public:
   typedef std::unordered_map<std::string, std::shared_ptr<ast::Node>> NodeMap;
+  typedef std::list<std::string> NodeList;
 
   ScopeType type;
-  NodeMap nodes[static_cast<int>(ast::Node::Type::Count_)];
+  NodeMap nodeMaps[static_cast<int>(ast::Node::Type::Count_)];
+  NodeList keyLists[static_cast<int>(ast::Node::Type::Count_)];
 };
 
 Scope::Scope(ScopeType type) : dPtr_(new ScopePrivate(this)) {
@@ -25,11 +28,14 @@ void Scope::print() const {
   SUCKC_D();
 
   std::cout << "SCOPE_BEGIN(" << d->type << ")" << std::endl;
-  for (auto &nodeMap : d->nodes) {
-    for (auto &node : nodeMap) {
-      std::cout << "NODE(" << (int)node.second->getType() << "):"
-                << "name(" << node.first
-                << "):source: " << node.second->toSource() << std::endl;
+  for (int i = 0; i < static_cast<int>(ast::Node::Type::Count_); ++i) {
+    auto &nodeMap = d->nodeMaps[i];
+    auto &keyList = d->keyLists[i];
+    for (auto &key : keyList) {
+      auto node = nodeMap[key];
+      std::cout << "NODE(" << (int)node->getType() << "):"
+                << "name(" << node->getName()
+                << "):source: " << node->toSource() << std::endl;
     }
   }
   std::cout << "SCOPE_END" << std::endl;
@@ -43,7 +49,7 @@ ScopeType Scope::getType() const {
 std::shared_ptr<ast::Node> Scope::findNode(const NodeType type,
                                            const std::string &name) {
   SUCKC_D();
-  auto nodeMap = &d->nodes[static_cast<int>(type)];
+  auto nodeMap = &d->nodeMaps[static_cast<int>(type)];
   auto it = nodeMap->find(name);
   if (it != nodeMap->end())
     return it->second;
@@ -54,7 +60,11 @@ std::shared_ptr<ast::Node> Scope::findNode(const NodeType type,
 void Scope::addNode(const std::string &name,
                     const std::shared_ptr<ast::Node> &node) {
   SUCKC_D();
-  auto &nodeMap = d->nodes[static_cast<int>(node->getType())];
+  auto &nodeMap = d->nodeMaps[static_cast<int>(node->getType())];
+  auto &keyList = d->keyLists[static_cast<int>(node->getType())];
+  if (nodeMap.find(name) == nodeMap.end()) {
+    keyList.push_back(name);
+  }
   nodeMap[name] = node;
 }
 
